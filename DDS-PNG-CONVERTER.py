dfee60908419c1519f550a5d580a73987394ac97
ВарКомНадзор <artemdubrovin2artem@yandex.ru>
2025-01-10 23:44:46 +0300

    Update DDS-PNG-CONVERTER.py

--- DDS-PNG-CONVERTER.py
+++ DDS-PNG-CONVERTER.py
@@ -5,14 +5,14 @@ from PIL import Image
 dds_img = [f for f in os.listdir('.') if f.endswith('.dds')]
 
 # Creating a folder to save png files
-folder = 'PNG'
-if not os.path.exists(folder):
-    os.makedirs(folder)
+Name_Folder = "PNG"
+if not os.path.exists(Name_Folder):
+    os.makedirs(Name_Folder)
 
 # Convert and save each dds file in png format
 for file in dds_img:
     img = Image.open(file)
-    img.save(os.path.join(folder, os.path.splitext(file)[0] + '.png'))
+    img.save(os.path.join(Name_Folder, os.path.splitext(file)[0] + '.png'))
     print(F"{file} --> png")
 
 input("The files have been converted. Press [Enter] to close the program...")
